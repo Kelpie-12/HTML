5320c7c35d4b4b0b9ac82440ccd7f3c1ffe97174
roman <roman121998yarkov@gmail.com>
2025-04-07 12:56:26 +0400

    2048

---
+++ 11-JQuery/script-jqu.js
@@ -0,0 +1,23 @@
+// JavaScript source code
+$(document).ready(
+    function () {
+        /*$("#hide").click(
+        //    function () {
+        //        $("div[style]").hide(1000);
+        //        $("p").hide(1000);
+        //    }
+        //)
+        //$("#show").click(
+        //    function () {
+        //        $("div[style]").show(1000);
+        //        $("p").show(1000);
+        //    }
+        )*/
+        $("#tumbler").click(
+            function () {
+                $("div[style]").toggle();
+            }
+        )
+    }
+
+)
